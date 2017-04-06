a4e352962de96d5ae652776d12bde6d4db504ac7
Makc Muff <mmuffin79@gmail.com>
2017-04-06 21:44:47 +0300

    Delete PostExecuteCallback

--- app/src/main/java/com/example/muffin/weather/PostExecuteCallback.java
+++
@@ -1,9 +0,0 @@
-package com.example.muffin.weather;
-
-
-import com.example.muffin.weather.GsonModels.WeatherForecast;
-
-public interface PostExecuteCallback {
-
-    void postExecute(WeatherForecast forecast);
-}
